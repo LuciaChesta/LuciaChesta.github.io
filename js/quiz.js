28e1016e4d2a43fbe7f88d1813f8eca18deaeb7a
LuciaChesta <75315865+LuciaChesta@users.noreply.github.com>
2026-02-10 01:36:05 -0300

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,37 +1,47 @@
-let currentQuestion = 1;
+let questions = [
+  "Question 1",
+  "Question 2",
+  "Question 3",
+  "Question 4",
+  "Question 5"
+];
+
+let currentQuestion = 0;
 let timeLeft = 10;
 let timer;
 
-function startTimer() {
-    timeLeft = 10;
-    document.getElementById("time").innerText = timeLeft;
+function loadQuestion() {
+  document.getElementById("question").innerText = questions[currentQuestion];
+  timeLeft = 10;
+  document.getElementById("time").innerText = timeLeft;
 
-    timer = setInterval(() => {
-        timeLeft--;
-        document.getElementById("time").innerText = timeLeft;
-
-        if (timeLeft === 0) {
-            nextQuestion();
-        }
-    }, 1000);
+  clearInterval(timer);
+  timer = setInterval(countdown, 1000);
 }
 
-function selectAnswer() {
+function countdown() {
+  timeLeft--;
+  document.getElementById("time").innerText = timeLeft;
+
+  if (timeLeft <= 0) {
     nextQuestion();
+  }
+}
+
+function answer() {
+  nextQuestion();
 }
 
 function nextQuestion() {
-    clearInterval(timer);
+  clearInterval(timer);
+  currentQuestion++;
 
-    currentQuestion++;
-
-    if (currentQuestion > 5) {
-        document.querySelector(".quiz-container").innerHTML = "<h2>Quiz finished!</h2>";
-        return;
-    }
-
-    document.getElementById("question").innerText = "Question " + currentQuestion;
-    startTimer();
+  if (currentQuestion < questions.length) {
+    loadQuestion();
+  } else {
+    document.querySelector(".quiz-box").innerHTML =
+      "<h2>Quiz finished 🎉</h2>";
+  }
 }
 
-startTimer();
+loadQuestion();
